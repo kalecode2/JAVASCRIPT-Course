74dfe692a820bbda6ee48f15863aa9271b2757e9
kalecode2 <kalecode2@gmail.com>
2025-01-20 22:47:32 +0400

    switch rock-paper-scissors and todo-list project adding addEventListener

--- script/12-todo-list.js
+++ script/12-todo-list.js
@@ -19,11 +19,9 @@ function renderTodoList() {
       const html = `
          <div>${name}</div>
          <div>${dueDate}</div>     
-         <button onclick="
-            todoList.splice(${index}, 1);
-            renderTodoList();
-         " class="delete-todo-button">Delete</button>
-         
+         <button class="delete-todo-button js-delete-todo-button">
+            Delete
+         </button>
       `;
       todoListHTML += html;
    });
@@ -46,8 +44,21 @@ function renderTodoList() {
    } */
 
    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+   document.querySelectorAll('.js-delete-todo-button')
+      .forEach();
 }
 
+// delete element 1 by 1 from a render list.
+document.querySelectorAll('.js-add-todo-button')
+   .addEventListener('click', () => {
+      addTodo((deleteButton, index) => {
+         deleteButton.addEventListener('click', () => {
+            todoList.splice(index, 1);
+            renderTodoList();
+         });
+      });
+   });
 
 function addTodo() {
    const inputElement = document.querySelector('.js-name-input');
