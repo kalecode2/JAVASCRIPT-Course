b0cb74483b6760eb64e01cedfe76710cc22ac510
kalecode2 <kalecode2@gmail.com>
2025-01-20 18:19:23 +0400

    update rock-paper-scissors-game-with-advanced-fucntions

--- script/12-rock-paper-scissors.js
+++ script/12-rock-paper-scissors.js
@@ -17,9 +17,10 @@ updateScoreElement();
 let isAutoPlaying = false;
 let intervalId;
 
+/* const autoPlay = () => {}; */
 function autoPlay() {
    if (!isAutoPlaying) {
-      intervalId = setInterval(function() {
+      intervalId = setInterval(() => {
          const playerMove = pickComputerMove();
          playGame(playerMove);
       }, 1000);
@@ -28,7 +29,20 @@ function autoPlay() {
       clearInterval(intervalId);
       isAutoPlaying = false;
    }
-}   
+} 
+
+document.querySelector('.js-rock-button')
+   .addEventListener('click', () => {
+      playGame('rock');
+   });
+document.querySelector('.js-paper-button')
+   .addEventListener('click', () => {
+      playGame('paper');
+   });
+document.querySelector('.js-scissors-button')
+   .addEventListener('click', () => {
+      playGame('scissors');
+   });
 
 function playGame(playerMove) {                                                                                                                                                                                                                                       
    const computerMove = pickComputerMove();
---
+++ script/12-todo-list.js
@@ -0,0 +1,69 @@
+const todoList = [
+   {
+      name: 'make dinner',
+      dueDate: '19-01-2025'
+   }, 
+   {
+      name: 'wash dishes',
+      dueDate: '28-01-2025'
+   }
+];
+
+renderTodoList();
+
+function renderTodoList() {
+   let todoListHTML = '';
+
+   todoList.forEach((todoObject, index) => {
+      const { name, dueDate } = todoObject;
+      const html = `
+         <div>${name}</div>
+         <div>${dueDate}</div>     
+         <button onclick="
+            todoList.splice(${index}, 1);
+            renderTodoList();
+         " class="delete-todo-button">Delete</button>
+         
+      `;
+      todoListHTML += html;
+   });
+
+   /* for (let i = 0; i < todoList.length; i++) {
+      const todoObject = todoList[i];
+      // const name = todoObject.name;
+      // const dueDate = todoObject.dueDate;
+      const { name, dueDate } = todoObject;
+      const html = `
+         <div>${name}</div>
+         <div>${dueDate}</div>     
+         <button onclick="
+            // todoList.splice(${i}, 1);
+            renderTodoList();
+         " class="delete-todo-button">Delete</button>
+         
+      `;
+      // todoListHTML += html;
+   } */
+
+   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+
+function addTodo() {
+   const inputElement = document.querySelector('.js-name-input');
+   const name = inputElement.value;
+
+   const dateInputElement = document.querySelector('.js-due-date-input');
+   const dueDate = dateInputElement.value;
+
+   todoList.push({
+      // name: name,
+      // dueDate: dueDate
+      name,
+      dueDate
+   });
+
+   inputElement.value = '';
+
+   renderTodoList();
+}
